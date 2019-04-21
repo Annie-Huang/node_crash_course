2b33fa00ca2cac9eab4ba21f0d92d783afeb4dc4
Annie Huang <annie.hn@gmail.com>
2019-04-21 20:48:20 +1000

    Update notes: different between CommonJS, AMD, and ES6

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+// CommonJS syntax.
 const Person = require('./person');
+
+// AMD syntax:
+// require(['./printmodule'], function(printModule){
+//    printModule.printMessage('Hello Amd');
+// });
+
+// Node has been use this yet, if you want to use the following ES6 syntax you will need to use babel
+// import Person from './person';
 
 const person1 = new Person('John Doe', 30);
 
