eded92e604d4677bf505da79bbeff84d718297a2
Annie Huang <annie.hn@gmail.com>
2019-04-21 20:28:11 +1000

    Update notes

--- person.js
+++ person.js
@@ -5,6 +5,18 @@
 //
 // module.exports = person;
 
+// ##############################################
+
+// What got wrapped around the class definitely is the following...
+// Module Wrapper Function
+// (function (exports, require, module, __filename, __dirname) {
+
+// })
+
+// console.log(__dirname, __filename);
+// __dirname = D:\react\node_crash_course
+// __filename = D:\react\node_crash_course\person.js
+
 class Person {
     constructor(name, age) {
         this.name = name;
