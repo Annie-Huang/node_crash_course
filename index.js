5e33605d3a8a6cf1072de5641d469d21041d271a
Annie Huang <annie.hn@gmail.com>
2019-05-11 16:26:38 +1000

    set up the basic structure of a web server (without using library like express)

--- index.js
+++ index.js
@@ -24,3 +24,17 @@
 // logger.log('Hello World');
 // logger.log('Hi');
 // logger.log('Hello');
+
+// -----------------------------------------------------
+
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
