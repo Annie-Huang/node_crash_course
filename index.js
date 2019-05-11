53af219afa5e66c7749ce6f2cec8730935781822
Annie Huang <annie.hn@gmail.com>
2019-05-11 17:03:31 +1000

    Serving JSON.

--- index.js
+++ index.js
@@ -52,6 +52,15 @@ const server = http.createServer((req, res) => {
         });
     }
 
+    if(req.url === '/api/users') {
+        const users = [
+            {name: 'Bob Smith', age: 40},
+            {name: 'Bob Smith', age: 30}
+        ];
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(users));
+    }
+
 });
 
 const PORT = process.env.PORT || 5000;
