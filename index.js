6045432d2d673403a30b527f43f6de22714afeb1
Annie Huang <annie.hn@gmail.com>
2019-05-11 16:59:19 +1000

    Add about page.

--- index.js
+++ index.js
@@ -42,8 +42,14 @@ const server = http.createServer((req, res) => {
             // res.end('<h1>Home</h1>');
             res.end(content);
         });
+    }
 
-
+    if(req.url === '/about') {
+        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(content);
+        });
     }
 
 });
