26d74904df0ec9b7bde30e573455187477d45a39
Annie Huang <annie.hn@gmail.com>
2019-05-11 16:14:34 +1000

    Back up logger.js

--- index.js
+++ index.js
@@ -13,12 +13,14 @@
 //
 // person1.greeting();
 
-const Logger = require('./logger');
+// -----------------------------------------------------
 
-const logger = new Logger();
-
-logger.on('message', (data) => console.log('Called Listener', data));
-
-logger.log('Hello World');
-logger.log('Hi');
-logger.log('Hello');
+// const Logger = require('./logger');
+//
+// const logger = new Logger();
+//
+// logger.on('message', (data) => console.log('Called Listener', data));
+//
+// logger.log('Hello World');
+// logger.log('Hi');
+// logger.log('Hello');
